54da0f7fa16e9cfdc3a5674ac81053e496b230ea
tuzinmaibrabo <arthurquarxma@outlook.com>
2023-12-12 18:06:26 -0300

    second commit

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ function calculate(number) {
   for (let i = 0; i < number; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
       result += i;
+      console.log("Valores inteiros divisíveis por 3 ou 5: " + i);
     }
   }
   return result;
 }
 
+// Para testar, troque o 10 pelo número desejado!
 console.log(calculate(10));
